a62ef5d25ccd77e4f475888878716e01208dd6f8
Daniel Deutsch <deudan1010@gmail.com>
2017-05-12 21:13:27 +0200

    add mongoose and connect to mongo localhost

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
 
 // serve static files from /public
 app.use(express.static(__dirname + '/template'));
@@ -9,7 +21,7 @@ var routes = require('./routes/router');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
@@ -17,7 +29,7 @@ app.use(function(req, res, next) {
 
 // error handler
 // define as the last app.use callback
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json('error', {
     message: err.message,
