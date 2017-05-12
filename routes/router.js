4085d249ff9651f90bcd911d12132d14e65b9d93
Daniel Deutsch <deudan1010@gmail.com>
2017-05-12 21:41:59 +0200

    add POST route

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 
-// GET /
-router.get('/', function(req, res, next) {
-  return res.sendFile(path.join(__dirname+'/template/index.html'));
+// GET route for reading data
+router.get('/', function (req, res, next) {
+  return res.sendFile(path.join(__dirname + '/template/index.html'));
+});
+
+//POST route for updating data
+router.post('/', function (req, res, next) {
+  if (req.body.email &&
+    req.body.password) {
+      
+     } else {
+    var err = new Error('Alle field have to be filled out');
+    err.status = 400;
+    return next(err);
+  }
+
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.confirmPassword) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    return next(err);
+  }
 });
 
 
