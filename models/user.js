33ac4662c38f7c3115615983cf60effe2ebbd7ed
Daniel Deutsch <deudan1010@gmail.com>
2017-05-13 14:15:21 +0200

    add bcrypt package, refactor and fix  router, add hashing in the mongoose schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
 var UserSchema = new mongoose.Schema({
   email: { 
     type: String, 
@@ -21,6 +23,20 @@ var UserSchema = new mongoose.Schema({
     required: true,
   }
 });
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
 
--- routes/router.js
+++ routes/router.js
@@ -9,12 +9,19 @@ router.get('/', function (req, res, next) {
 });
 
 // GET route after registering
-router.get('/register', function (req, res, next) {
-  return res.send('GET registered');
+router.get('/profile', function (req, res, next) {
+  return res.send('GET profile');
 });
 
 //POST route for updating data
 router.post('/', function (req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    return next(err);
+  }
+
   if (req.body.email &&
     req.body.username &&
     req.body.password &&
@@ -37,22 +44,16 @@ router.post('/', function (req, res, next) {
     });
 
   } else {
-    var err = new Error('Alle field have to be filled out');
+    var err = new Error('All fields have to be filled out');
     err.status = 400;
     return next(err);
   }
 
-  // confirm that user typed same password twice
-  if (req.body.password !== req.body.confirmPassword) {
-    var err = new Error('Passwords do not match.');
-    err.status = 400;
-    return next(err);
-  }
 });
 
 // POST route after registering
-router.post('/register', function (req, res, next) {
-  return res.send('POST registered');
+router.post('/profile', function (req, res, next) {
+  return res.send('POST profile');
 });
 
 
